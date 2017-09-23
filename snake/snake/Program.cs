874c9171f05f607a85a8fb0a7363b33d2d4da293
Лиза Буревестник < >
2017-09-24 00:22:48 +0300

    ррр

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,15 +10,17 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            point p1 = new point(1, 2, '!');
-                    
-            p1.draw(p1.x, p1.y, p1.sym);
-
-          
-            point p2 = new point(2,4, '#');
-            
-            p2.draw(p2.x, p2.y, p2.sym);
-
+            //Console.SetBufferSize(80, 25);
+            gorizline upline = new gorizline(0, 78, 0, '+');
+            gorizline dline = new gorizline(0, 78, 24, '+');
+            vertline lline = new vertline(0, 24, 0, '+');
+            vertline rline = new vertline(0, 24, 78, '+');
+            upline.drow();
+            dline.drow();
+            lline.drow();
+            rline.drow();
+            point p = new point(4, 5, '*');
+            p.draw();
             Console.ReadLine();
 
         }
