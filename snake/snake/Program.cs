71511693c7f648d66b8eff938eff733d5c61e98c
Лиза Буревестник < >
2017-09-23 13:00:27 +0300

    construct

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,36 +10,19 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            point p1 = new point();
-            p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';
-          
+            point p1 = new point(1, 2, '!');
+                    
             p1.draw(p1.x, p1.y, p1.sym);
 
-            //int x1 = 1;
-            //int y1 = 3;
-            //char symb = '*';
-            /*int x2 = 4;
-            int y2 = 5;
-            char symb2 = '#';
-            draw(x2, y2, symb2);
-            */
-            point p2 = new point();
-            p2.x = 5;
-            p2.y = 2;
-            p2.sym = '#';
-
+          
+            point p2 = new point(2,4, '#');
+            
             p2.draw(p2.x, p2.y, p2.sym);
 
             Console.ReadLine();
 
         }
 
-        /*static void draw(int a, int b, char s)
-        {
-            Console.SetCursorPosition(a, b);
-            Console.Write(s);
-        }*/
+       
     }
 }
