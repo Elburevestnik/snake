4858c8e3da4f9f204473078cb98f72f7f3724cfe
Лиза Буревестник < >
2017-09-24 11:41:50 +0300

    родилась змейка и она двигается!

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace snake
@@ -10,7 +11,9 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            //Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
             gorizline upline = new gorizline(0, 78, 0, '+');
             gorizline dline = new gorizline(0, 78, 24, '+');
             vertline lline = new vertline(0, 24, 0, '+');
@@ -20,8 +23,24 @@ namespace snake
             lline.drow();
             rline.drow();
             point p = new point(4, 5, '*');
-            p.draw();
-            Console.ReadLine();
+    
+            snake sn = new snake(p, 4, direction.RIGHT);
+            sn.drow();
+            sn.Move();
+            Thread.Sleep(300);
+         
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    sn.handlykey(key.Key);
+                }
+                
+                Thread.Sleep(100);
+                sn.Move();
+            }
+            //Console.ReadLine();
 
         }
 
